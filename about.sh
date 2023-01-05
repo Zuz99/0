70a86419486db9cc722bbe1aa6ba47cdabfd8755
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-01-05 15:25:56 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#         Premium Auto Script By Rhyuu          #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By Aixxy7                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Kenn Hiroyuki                                         #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
---
+++ edu.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# Proxy For Edukasi Ssh & Ovpn Websocket
+# ====================================
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/edussh-nontls https://raw.githubusercontent.com/kenDevXD/0/main/proxy-template.py
+chmod +x /usr/local/bin/edussh-nontls
+
+# Installing Service
+cat > /etc/systemd/system/edussh-nontls.service << END
+[Unit]
+Description=Ssh Websocket By Akhir Zaman
+Documentation=https://xnxx.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/edussh-nontls 2082
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable edussh-nontls
+systemctl restart edussh-nontls
+
+clear
+
+# Installing Service
+cat > /etc/systemd/system/edussh-tls.service << END
+[Unit]
+Description=Ssh Websocket By Akhir Zaman
+Documentation=https://xnxx.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/edussh-nontls
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable edussh-tls
+systemctl restart edussh-tls
+
+clear
+
+# Ovpn Websocket !!!
+# =================================
+
+# Getting Ovpn Ws Template
+wget -q -O /usr/local/bin/edu-ovpn https://raw.githubusercontent.com/kenDevXD/0/main/edu-op.py
+chmod +x /usr/local/bin/edu-ovpn
+
+# Installing Service Ovpn Websocket
+cat > /etc/systemd/system/edu-ovpn.service << END
+[Unit]
+Description=Ovpn Websocket By Akhir Zaman
+Documentation=https://xnxx.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-ovpn 2095
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable edu-ovpn
+systemctl restart edu-ovpn
+
+clear
+echo -e "==============================="
+echo -e "Done Install Ssh & Ovpn Websocket"
+echo -e "==============================="
+echo -e "PORT SSH WS NON TLS          : 2082"
+echo -e "PORT SSH WS SSL              : 443"
+echo -e "PORT OVPN WS B               : 2095"
+echo -e "==============================="
+echo -e "Script By Kenn Hiroyuki"
